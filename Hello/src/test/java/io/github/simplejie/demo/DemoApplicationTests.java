2fa9f34711a8e4d653b95af84f51a7555097760d
Xingbo.Jie <jiexingbo@mozat.com>
2018-11-22 17:19:47 +0800

    hello test

--- Hello/src/test/java/io/github/simplejie/demo/DemoApplicationTests.java
+++ Hello/src/test/java/io/github/simplejie/demo/DemoApplicationTests.java
@@ -1,16 +1,40 @@
 package io.github.simplejie.demo;
 
+import io.github.simplejie.demo.web.HelloController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = MockServletContext.class)
+@WebAppConfiguration
 public class DemoApplicationTests {
 
+    private MockMvc mvc;
+
+    @Before
+    public void setup() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
     @Test
-    public void contextLoads() {
+    public void testHelloRequest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello, Spring"));
     }
 
 }
